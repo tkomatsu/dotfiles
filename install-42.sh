0c4b26cf75e5b41e23a69b89513f950bbf4ed50f
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-02-03 10:26:45 +0900

    fix: cp -r

--- install-42.sh
+++ install-42.sh
@@ -10,16 +10,15 @@ bundle
 
 # 42 header
 cd $HOME
-mkdir ~/.vim/plugin
-cp dotfiles/stdheader.vim ~/.vim/plugin/
+cp -r dotfiles/stdheader.vim ~/.vim/plugin/
 
 # minilibx-linux
 if [ $(uname) = Linux ]; then 
 	sudo apt-get install gcc make xorg libxext-dev libbsd-dev
 	cd $HOME/dotfiles/minilibx-linux
 	make 
-	cp libmlx.a /usr/local/lib/libmlx.a
-	cp libmlx_$(HOSTNAME).a /usr/local/lib/libmlx_$(HOSTNAME).a
-	cp mlx.h /usr/local/include
-	cp man/man3/mlx*.1 /usr/local/man/man3
+	cp -r libmlx.a /usr/local/lib/libmlx.a
+	cp -r libmlx_$(HOSTNAME).a /usr/local/lib/libmlx_$(HOSTNAME).a
+	cp -r mlx.h /usr/local/include
+	cp -r man/man3/mlx*.1 /usr/local/man/man3
 fi
