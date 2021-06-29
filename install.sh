39175d494be84c76a6aa3971c8b8741576967e63
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-06-29 11:12:39 +0900

    update

--- install.sh
+++ install.sh
@@ -5,16 +5,6 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 echo "start setup..."
 cd $HOME
 
-# install Vundle for vim plugin
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-for file in .vimrc .zshrc .gitconfig .gitignore .tmux.conf
-do
-	[ ! -e $file ] && ln -s dotfiles/$file .
-done
-
-echo 'export PATH=~/homebrew/bin:$PATH' >> ~/.zshrc.local
-
 echo "installing Homebrew ..."
 which brew >/dev/null 2>&1 || cd $HOME; curl https://brew.42.fr | zsh
 
@@ -30,6 +20,14 @@ echo "ok. run brew upgrade ..."
 brew upgrade
 brew install font-hack-nerd-font --cask
 brew cleanup
+
+# install Vundle for vim plugin
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+for file in .vimrc .zshrc .gitconfig .gitignore .tmux.conf
+do
+	[ ! -e $file ] && ln -s dotfiles/$file .
+done
 
 # config
 mkdir -p $HOME/.config/gh & ln -s $HOME/dotfiles/gh/config.yml $HOME/.config/gh/config.yml
